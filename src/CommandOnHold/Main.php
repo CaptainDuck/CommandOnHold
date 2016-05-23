073d2b9f4f33d1ec7bf27f06643c2bdb5db1f1af
CaptainDuck <jmdimayuga021@gmail.com>
2016-05-23 13:59:33 +0800

    Update Main.php

--- src/CommandOnHold/Main.php
+++ src/CommandOnHold/Main.php
@@ -1,7 +1,12 @@
->?php
+<?php
 
-namespace CaptainDuck/CommandOnHold
+namespace CaptainDuck\CommandOnHold;
 
-use pocketmine/plugin/PluginBase
+use pocketmine\plugin\PluginBase;
+use pocketmine\item\Item;
+use pocketmine\event\Listener;
+use pocketmine\player;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat as C;
 
-class Main extends PluginBase
+class Main extends PluginBase extends Listener{
