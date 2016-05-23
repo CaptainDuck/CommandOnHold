f15dba6c125d53e0b1edf141ffd024f22095ac0a
CaptainDuck <jmdimayuga021@gmail.com>
2016-05-23 15:30:12 +0800

    Update Main.php

--- src/CommandOnHold/Main.php
+++ src/CommandOnHold/Main.php
@@ -9,12 +9,13 @@ use pocketmine\command;
 use pocketmine\permission\Permission;
 use pocketmine\permission\Permissible;
 use pockemine\command\CommandExecutor
-use pocketmine\Server
+use pocketmine\Server;
+use pocketmine\event\player
 use pocketmine\player;
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat as C;
 
-class Main extends PluginBase implements Listener{
+class Main extends PlayerEvent implements Cancellable{
   
   public function onEnable(){
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
@@ -30,3 +31,4 @@ class Main extends PluginBase implements Listener{
   //Start
     if($item->getId() == 347){
       $player->sendPopup("You're holding a Clock!");
+      $player->sendMessage("Your name is " . $player->getName() . "!"")
