6d7ae0b25235a24a2195fb4d5804f55d706094fb
CaptainDuck <jmdimayuga021@gmail.com>
2016-05-24 17:00:15 +0800

    Add files via upload

---
+++ src/Main.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace CaptainDuck\CommandOnHold;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\item\Item;
+use pocketmine\event\Listener;
+use pocketmine\command;
+use pocketmine\permission\Permission;
+use pocketmine\permission\Permissible;
+use pockemine\command\CommandExecutor;
+use pocketmine\Server;
+use pocketmine\player;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat as C;
+
+class Main extends PlayerEvent implements Cancellable{
+  
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    $this->getServer()->getLogger()->info("CommandOnHold by CaptainDuck enabled!");
+  }
+  
+  public function onPlayerItemHeldEvent(){
+    $player = $event->getPlayer();
+    $item = $player->getInventory()->getItemInHand();
+    $cmd = Your name is " . $playername() . "!";
+    $playername = " . $player->getName() . ";
+    $execute =  $this->getServer()->dispatchCommand($player, '$cmd');
+    switch ($item->getId()){
+      case 264:
+        $player->sendMessage("You're holding a Diamond!");
+        $player->sendMessage("$cmd");
+  
+  public function onDisable(){
+    $this->getServer()->getLogger()->info("CommandOnHold by CaptainDuck disabled! :o");
+  }
